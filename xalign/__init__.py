3a109590aec89354136131040b9f705fb865a951
Alexander Lachmann <32603869+lachmann12@users.noreply.github.com>
2021-10-06 17:16:27 -0400

    sa

--- xalign/__init__.py
+++ xalign/__init__.py
@@ -32,21 +32,22 @@ def retrieve_ensembl_organisms():
     return organisms
 
 def build_index(aligner: str, species: str, overwrite=False):
-    if (not os.path.exists(filehandler.get_data_path()+"index/"+species+".idx")) or overwrite:
-        organisms = retrieve_ensembl_organisms()
-        if species in organisms:
-            print("Download fastq.gz")
-            print(filehandler.get_data_path())
-            filehandler.download_file(organisms[species][2], "temp.fastq.gz")
-            #print("Download gtf")
-            #filehandler.download_file(organisms[species][3], "temp.gtf")
-        else:
-            sys.exit(0)
-        osys = platform.system().lower()
-        download_aligner(aligner, osys)
-        os.makedirs(filehandler.get_data_path()+"/index", exist_ok=True)
-        print(filehandler.get_data_path()+"/kallisto/kallisto index -i "+filehandler.get_data_path()+"index/"+species+".idx "+filehandler.get_data_path()+"temp.fastq.gz")
-        os.system(filehandler.get_data_path()+"/kallisto/kallisto index -i "+filehandler.get_data_path()+"index/"+species+".idx "+filehandler.get_data_path()+"temp.fastq.gz")
+    organisms = retrieve_ensembl_organisms()
+    if species in organisms:
+        print("Download fastq.gz")
+        print(filehandler.get_data_path())
+        filehandler.download_file(organisms[species][2], "temp.fastq.gz")
+        #print("Download gtf")
+        #filehandler.download_file(organisms[species][3], "temp.gtf")
+    else:
+        sys.exit(0)
+    osys = platform.system().lower()
+    download_aligner(aligner, osys)
+    os.makedirs(filehandler.get_data_path()+"/index", exist_ok=True)
+    if aligner == "kallisto":
+        if (not os.path.exists(filehandler.get_data_path()+"index/"+species+".idx")) or overwrite:
+            print("Build kallisto index for "+species)
+            os.system(filehandler.get_data_path()+"/kallisto/kallisto index -i "+filehandler.get_data_path()+"index/"+species+".idx "+filehandler.get_data_path()+"temp.fastq.gz")
     else:
         print("Index already exists. Use overwrite to rebuild.")
 
@@ -54,8 +55,17 @@ def download_aligner(aligner, osys):
     
     print(osys)
 
+    if aligner == "salmon":
+        if osys == "linux":
+            url = "https://github.com/COMBINE-lab/salmon/releases/download/v1.5.2/salmon-1.5.2_linux_x86_64.tar.gz"
+            filepath = filehandler.download_file(url, "salmon.tar.gz")
+            file = tarfile.open(filepath)
+            file.extract('salmon-1.5.2_linux_x86_64\bin\salmon', filehandler.get_data_path())
+            file.close()
+        else:
+            print("Salmon not supported by this package for this operating system.")
+
     if aligner == "kallisto":
-        url = ""
         if osys == "windows":
             url = "https://github.com/pachterlab/kallisto/releases/download/v0.46.1/kallisto_windows-v0.46.1.zip"
             filepath = filehandler.download_file(url, "kallisto.zip")
