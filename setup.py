05d819bcd12614a6f04f48cbfefb594bd6bc08e2
Alexander Lachmann <32603869+lachmann12@users.noreply.github.com>
2021-10-06 18:04:11 -0400

    ok

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="xalign",
-    version="0.1.10",
+    version="0.1.11",
     author="Alexander Lachmann",
     author_email="alexander.lachmann@mssm.edu",
     description="Alignment in a python wrapper.",
