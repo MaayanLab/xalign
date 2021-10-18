cbd2333412c04d05b646bd4b8e3e8edcc943e0aa
Alexander Lachmann <32603869+lachmann12@users.noreply.github.com>
2021-10-18 16:55:19 -0400

    Update __init__.py

--- xalign/__init__.py
+++ xalign/__init__.py
@@ -145,6 +145,7 @@ def align_folder(aligner, species, folder, t=1, identifier="symbol", overwrite=F
             bnames = [os.path.basename(x) for x in fq]
             sample_names.append(re.sub(r'_$','',os.path.commonprefix(bnames)))
         res = align_fastq(aligner, species, fq, t=t, overwrite=overwrite, verbose=verbose)
+        print(res)
         transcript_counts.append(res.loc[:,"reads"])
         res_gene = ensembl.agg_gene_counts(res, species, identifier=identifier)
         gene_counts.append(res_gene.loc[:,"counts"])
