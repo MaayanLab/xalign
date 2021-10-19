d5c8002f3b046bcde80e25e4375dd7efef4f77b5
Alexander Lachmann <32603869+lachmann12@users.noreply.github.com>
2021-10-18 20:37:39 -0400

    fix permission

--- xalign/sra.py
+++ xalign/sra.py
@@ -1,14 +1,30 @@
 import subprocess
 import multiprocessing
 from tqdm import tqdm
+import gzip
+import os
 
 import xalign.file as filehandler
+
+def gunzip(source_filepath, dest_filepath, block_size=65536):
+    with gzip.open(source_filepath, 'rb') as s_file, \
+            open(dest_filepath, 'wb') as d_file:
+        while True:
+            block = s_file.read(block_size)
+            if not block:
+                break
+            else:
+                d_file.write(block)
 
 def load_sra_star(args):
     load_sra(*args)
 
 def load_sra(sample, output):
-    res = subprocess.Popen(filehandler.get_data_path()+"fasterq-dump  --mem 2G --split-3 --threads 2 --skip-technical -O "+output+" "+sample, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+
+    if not os.path.exists(filehandler.get_data_path()+"fasterq-dump"):
+        gunzip(filehandler.get_data_path()+"fasterq-dump.gz", filehandler.get_data_path()+"fasterq-dump")
+
+    res = subprocess.Popen(filehandler.get_data_path()+"fasterq-dump -f --mem 2G --split-3 --threads 2 --skip-technical -O "+output+" "+sample, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     if res.wait() != 0:
         output, error = res.communicate()
         print(error)
